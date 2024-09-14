588f5caae1fa1cd0edfaf31195b0fb8058bec580
fabian <fabianjuliansyah89@gmail.com>
2024-09-14 08:59:41 +0700

    fabian commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,15 @@
-const js = require("@eslint/js");
-const globals = require('globals')
+import js from "@eslint/js";
 
-module.exports = [
+export default [
   js.configs.recommended,
 
   {
     rules: {
       "no-unused-vars": "warn",
-      "no-undef": "warn",
       "no-compare-neg-zero": "warn",
       "no-const-assign": "warn",
-      "no-control-regex": "warn"
+      "no-control-regex": "warn",
+      "arrow-body-style": "error",
     },
-    languageOptions: {
-      ecmaVersion: 2015,  // Atau bisa menggunakan 6
-      sourceType: "module"
-    }
-  }
+  },
 ];
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import express from 'express';
-import http from 'node:http';
+/*eslint no-undef: "off"*/
+import express from "express";
+import http from "node:http";
 
 const app = express();
-import mainRouter from './router.js';
+import mainRouter from "./router.js";
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-app.use('/', mainRouter);
+app.use(express.urlencoded({ extended: false }));
+app.use("/api", mainRouter);
 
 const server = http.createServer(app);
 
-server.listen(3000, 'localhost', function(){
-    console.log('Litening on localhost:3000');
+server.listen(3000, "localhost", function () {
+  console.log("Server listening in: http://localhost:3000");
 });
